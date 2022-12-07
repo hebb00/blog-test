2240d1918d87810bfe95bad68456bd085a3b7ce2
hebbs nator <hebbnator@gmail.com>
2022-12-07 22:01:02 +0300

    Initial commit from Create Next App

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer: React.FC = () => {
+  return (
+    <footer className="border-t border-gray-200 bg-gray-50 p-4">
+      <p className="text-center">© 2021 KITERETZ inc.</p>
+    </footer>
+  )
+}
+
+export default Footer
